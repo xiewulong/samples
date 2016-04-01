24aff9c630984db4187a5e7a778337dbce20bdf3
xiewulong <xiewulong@vip.qq.com>
2016-04-01 13:05:10 +0800

    samples

---
+++ js/sample.js
@@ -0,0 +1,31 @@
+/*!
+ * sample
+ * xiewulong <xiewulong@vip.qq.com>
+ * create: 2016/4/1
+ * version: 1.0.0
+ */
+
+/**
+ * sample module
+ * @module sample
+ * @since 1.0.0
+ */
+define(['jquery', 'bootstrap'], function($){
+	var	$win	= $(window),
+		$doc	= $(document),
+		Fn		= function(){},
+		exports	= {};
+
+	/**
+	 * sample
+	 * @method sample.sample
+	 * @since 1.0.0
+	 * @param {string} param 参数
+	 * @return {none}
+	 */
+	exports.sample = function(param){
+		console.dir(param);
+	};
+
+	return exports;
+});
