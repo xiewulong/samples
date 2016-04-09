993a6cc377cfecad16383b4406549eb3cb518a9e
xiewulong <xiewulong@vip.qq.com>
2016-04-09 23:41:50 +0800

    sample

--- js/sample.js
+++ js/sample.js
@@ -11,21 +11,49 @@
  * @since 1.0.0
  */
 define(['jquery', 'bootstrap'], function($) {
+
 	var	$win	= $(window),
 		$doc	= $(document),
 		Fn		= function() {},
 		exports	= {};
 
 	/**
-	 * sample
-	 * @method sample.sample
+	 * 获取get数据
+	 * @method sample.getQuerys
 	 * @since 1.0.0
-	 * @param {string} param 参数
-	 * @return {none}
+	 * @return {object}
 	 */
-	exports.sample = function(param) {
-		console.dir(param);
+	exports.getQuerys = function() {
+		return location.search ? new Function('return {' + location.search.substring(1).replace(/&/g, '",').replace(/=/g, ':"') + '"}')() : {};
 	};
 
+	/**
+	 * 开启模态框
+	 * @method $.modal
+	 * @since 1.0.0
+	 * @param {string} html html结构
+	 * @param {object} options bootstrap模态框参数
+	 * @return {object}
+	 */
+	$.modal = function(html, options){
+		return $(html).modal(options).on('hidden.bs.modal', function(){
+			$(this).remove();
+		});
+	};
+
+	/**
+	 * ajax全局配置
+	 * @since 1.0.0
+	 */
+	$.ajaxSetup({
+		dataType: 'json',
+		//crossDomain: true,
+		//xhrFields: {withCredentials: true},
+		error: function(xhr, status, error) {
+			console.log(error);
+		}
+	});
+
 	return exports;
+
 });
